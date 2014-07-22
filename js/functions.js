2ae1f51b482fc8f5c26d4577747869eb733b17c7
claus <claus.gehrke@web.de>
2014-07-23 00:13:10 +0200

    deleted jss folder

--- js/functions.js
+++ js/functions.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
         //var pathArray = window.location.pathname.split( '#' );
         //document.write(pathArray[0]);
     });
+// (function ($) {
+// 	// get proper height
+// 	$.fn.setHeight = function (thisContainer, thisHeaderHeight, thisContainerHeight) {
+// 		var minheight = $(thisContainer).height();
+// 		return $(this).css('height', gridheight / thisContainerHeight);
+// 	};
 
+// 	/*!
+// 	 * adapt height for fullheight grid
+// 	 * setHeight: get section height, subtract header height, divide container height
+// 	 */
+// 	$('.sortiment-1-1').setHeight('.section-sortiment', 150, 1);
