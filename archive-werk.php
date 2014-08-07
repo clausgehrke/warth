9759126c9e49d4885574b1eb9149341b9ace4edd
claus <claus.gehrke@web.de>
2014-08-07 02:02:44 +0200

    local merge part 2

--- archive-werk.php
+++ archive-werk.php
@@ -46,7 +46,7 @@ get_header();?>
     </div>
   </div>
   <div class="col-7-12 border_right">
-    <div class="content text">
+    <div class="text">
       <h2>Hallo</h2>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
       <a href="galleriedetail.html" class="btn_red btn-cont-text">Mehr</a>
