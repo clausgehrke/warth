3a10d60290ef73596b6960eb59a3165b5bd08a43
hofmannsven <info@hofmannsven.com>
2014-08-08 18:20:37 +0200

    Make 404 page translateable

--- 404.php
+++ 404.php
@@ -1,15 +1,18 @@
 <?php
 
 get_header();
- ?>
+
+?>
 <div class="grid main">
 	<div class="col-1-1">
 		<div class="border_left">
 			<div class="border_right">
         		<div class="content">
-						<h1>404 </h1>
-						<!-- so ein warth -->
-						<p>leider (noch) keine Seite verfügbar</p>
+						<h1>
+							<?php _e('Fehler 404', 'warth'); ?>
+						</h1>
+						<p>
+							<?php _e('Diese Seite ich leider nicht verfügbar.', 'warth'); ?></p>
 						<p style="line-height: 1.5;">
 							<?php echo '» <a href="' . HOME . '" class="red caps">' .__('Weiter zur Startseite', 'warth') . '</a>';?>
 						</p>
@@ -19,4 +22,5 @@ get_header();
 	</div>
 </div>
 <?php
+
 get_footer();
